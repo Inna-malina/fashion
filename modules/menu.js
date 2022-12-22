6aae8de02fad71ed037dfd64016b6c0b8bcfb9cc
Inna Cherckashina <68454245+Inna-malina@users.noreply.github.com>
2022-12-23 02:48:17 +0700

    Add files via upload

---
+++ modules/menu.js
@@ -0,0 +1,30 @@
+
+  let burgerClose = document.querySelector('.burger-close');
+  let burger = document.querySelector('.burger-menu');
+  let burgerMenu = document.querySelector('nav');
+
+  burger.addEventListener('click', function () {
+    console.log('qwerqwerqwer');
+    burgerMenu.classList.remove('menu-disactive');
+    burgerMenu.classList.add('menu-active');
+
+    burger.classList.remove('burger-active');
+    burger.classList.add('burger-disactive');
+
+    burgerClose.classList.remove('close-disactive');
+    burgerClose.classList.add('close-active');
+  });
+
+  burgerClose.addEventListener('click', function () {
+    burgerMenu.classList.remove('menu-active');
+    burgerMenu.classList.add('menu-disactive');
+
+    burger.classList.remove('burger-disactive');
+    burger.classList.add('burger-active');
+
+    burgerClose.classList.remove('close-active');
+    burgerClose.classList.add('close-disactive');
+  });
+
+  
+
