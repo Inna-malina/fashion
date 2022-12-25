aeb9660c5b781cc3d3958ebe99c591a82098c3c9
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2022-12-25 12:14:24 +0700

    feat: added section about me, script to menu

--- modules/menu.js
+++ modules/menu.js
@@ -1,30 +1,35 @@
+let burgerClose = document.querySelector('.burger-close');
+let burger = document.querySelector('.burger-menu');
+let burgerMenu = document.querySelector('nav');
 
-  let burgerClose = document.querySelector('.burger-close');
-  let burger = document.querySelector('.burger-menu');
-  let burgerMenu = document.querySelector('nav');
+burger.addEventListener('click', function () {
+  console.log('qwerqwerqwer');
+  burgerMenu.classList.remove('menu-disactive');
+  burgerMenu.classList.add('menu-active');
 
-  burger.addEventListener('click', function () {
-    console.log('qwerqwerqwer');
-    burgerMenu.classList.remove('menu-disactive');
-    burgerMenu.classList.add('menu-active');
+  burger.classList.remove('burger-active');
+  burger.classList.add('burger-disactive');
 
-    burger.classList.remove('burger-active');
-    burger.classList.add('burger-disactive');
+  burgerClose.classList.remove('close-disactive');
+  burgerClose.classList.add('close-active');
+});
 
-    burgerClose.classList.remove('close-disactive');
-    burgerClose.classList.add('close-active');
-  });
+burgerClose.addEventListener('click', function () {
+  burgerMenu.classList.remove('menu-active');
+  burgerMenu.classList.add('menu-disactive');
 
-  burgerClose.addEventListener('click', function () {
-    burgerMenu.classList.remove('menu-active');
-    burgerMenu.classList.add('menu-disactive');
+  burger.classList.remove('burger-disactive');
+  burger.classList.add('burger-active');
 
-    burger.classList.remove('burger-disactive');
-    burger.classList.add('burger-active');
+  burgerClose.classList.remove('close-active');
+  burgerClose.classList.add('close-disactive');
+});
 
-    burgerClose.classList.remove('close-active');
-    burgerClose.classList.add('close-disactive');
-  });
+let mainBunner = document.querySelector('.main-bunner');
+let mainBunnerHeight = window.getComputedStyle(mainBunner).height;
 
-  
-
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset >= mainBunner.scrollHeight) {
+    console.log('a');
+  }
+});
